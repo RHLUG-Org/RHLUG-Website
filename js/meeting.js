1a1fc17521be88270164b17ffb7d028117467733
siliconninja <theninja@thesilicon.ninja>
2021-09-01 14:52:33 -0400

    Add meeting location to JS

--- js/meeting.js
+++ js/meeting.js
@@ -1,4 +1,5 @@
 let url = 'https://raw.githubusercontent.com/RHLUG-Org/RHLUG-Meeting-JSON/master/rest.json';
 $.getJSON(url, function(data){
-    $("#meeting").html("Next Virtual Meeting: " + data.date + " "+ data.numeric_date+ " from " + data.time);
+    $("#meeting").html("Next Meeting: " + data.date + " "+ data.numeric_date+ " from " + data.time);
+    $("#meeting_location").html(data.location);
 });
